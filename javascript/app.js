93d921f1ec4663791bb5d8e2e078958c3ce3edea
LAUREL\Meleon <bbddstory@gmail.com>
2018-11-05 04:04:28 +1100

    Modified nav menu. Added pi page.

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Leon Li on 14/02/2017.
  */
-$(function(){
+$(function() {
     var canvasNav = document.getElementById('canvasNav'), ctxNav = canvasNav.getContext('2d'),
         diverY = 999, clientY = 150,
         imgBuoy = document.getElementById('imgBuoy'),
@@ -9,19 +9,24 @@ $(function(){
         imgFemaleAscent = document.getElementById('imgFemaleAscent'),
         imgPlate = document.getElementById('imgPlate');
 
-    $(window).on('load', function(){
+    $(window).on('load', function() {
         componentResize();
         draw(clientY); // initialise canvas
         initNavi();
         routing();
     });
 
-    window.addEventListener('mousemove', function(e){
+    window.addEventListener('mousemove', function(e) {
         clientY = e.clientY;
         draw();
     })
 
     window.addEventListener('resize', componentResize);
+
+    $('.pi').on('click', function() {
+        location.hash = 'pi';
+        routing();
+    })
 
     // resize nav canvas to full height
     function componentResize(){
@@ -31,68 +36,67 @@ $(function(){
         $('main').height($(window).height() - 20);
     }
 
-    function draw(){
+    function draw() {
         ctxNav.clearRect(0, 0, canvasNav.width, canvasNav.height);
 
         // Bottom plate
         ctxNav.globalAlpha = 0.1;
-        ctxNav.drawImage(imgPlate, 100, window.innerHeight - 98);
+        ctxNav.drawImage(imgPlate, 104, window.innerHeight - 98);
 
-        // Guide line
+        // Lanyard
         var guideLine = ctxNav.createLinearGradient(118, 129, 122, window.innerHeight - 87);
         guideLine.addColorStop(0, '#ebebeb');
         guideLine.addColorStop(1, '#00426e');
         ctxNav.fillStyle = guideLine;
         ctxNav.globalAlpha = 0.7;
-        ctxNav.fillRect(118, 129, 2, window.innerHeight - 129 - 87);
+        ctxNav.fillRect(123, 129, 2, window.innerHeight - 129 - 87);
 
         // Buoy
         ctxNav.globalAlpha = 1;
-        ctxNav.drawImage(imgBuoy, 58, 30);
+        ctxNav.drawImage(imgBuoy, 63, 30);
 
         // Diver
         ctxNav.globalAlpha = 1 - (clientY / window.innerHeight).toFixed(1);
-        if(clientY > diverY){
-            if(clientY < window.innerHeight - 220){
-                ctxNav.drawImage(imgDive, 70, clientY);
+        if(clientY > diverY) {
+            if(clientY < window.innerHeight - 220) {
+                ctxNav.drawImage(imgDive, 50, clientY);
                 diverY = clientY - 1;
-            }else{
-                ctxNav.drawImage(imgDive, 70, window.innerHeight - 220);
+            } else {
+                ctxNav.drawImage(imgDive, 50, window.innerHeight - 220);
                 diverY = window.innerHeight - 220;
             }
-        }else{
-            if(clientY > 150){
-                ctxNav.drawImage(imgFemaleAscent, 80, clientY);
+        } else {
+            if(clientY > 150) {
+                ctxNav.drawImage(imgFemaleAscent, 60, clientY);
                 diverY = clientY;
-            }else{
-                ctxNav.drawImage(imgFemaleAscent, 80, 150);
+            } else {
+                ctxNav.drawImage(imgFemaleAscent, 60, 150);
                 diverY = 150;
             }
         }
     }
 
-    function initNavi(){
-        $('#navi a').on('click', function(e){
+    function initNavi() {
+        $('#navi a').on('click', function(e) {
             $('main').load('views/' + e.target.hash.replace('#', '') + '.html', function() {
                 $(this).scrollTop(0);
             });
-
         })
     }
 
-    function routing(){
-        if(location.hash){
+    function routing() {
+        if(location.hash) {
             $('main').load('views/' + location.hash.replace('#', '') + '.html');
-        }else {
+        } else {
             $('main').load('views/moi.html');
         }
     }
 
-    function isMobile(){
-        try{
+    function isMobile() {
+        try {
             document.createEvent("TouchEvent");
             return true;
-        }catch(e){
+        } catch(e) {
             return false;
         }
     }
