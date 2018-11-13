3757d5ce855e0c1091163adb2f0d5a7b71c85779
UNICO-AUS\lli <leon.li@unico.com.au>
2018-11-13 14:00:44 +1100

    Added responsive code. Deploy for effect.

--- js/app.js
+++ js/app.js
@@ -2,108 +2,126 @@
  * Created by Leon Li on 14/02/2017.
  */
 $(function() {
-    var canvasNav = document.getElementById('canvasNav'), ctxNav = canvasNav.getContext('2d'),
-        diverY = 999, clientY = 150,
-        imgBuoy = document.getElementById('imgBuoy'),
-        imgDive = document.getElementById('imgDive'),
-        imgFemaleAscent = document.getElementById('imgFemaleAscent'),
-        imgPlate = document.getElementById('imgPlate');
+  var clientY = 150;
+  var diverY = 999;
 
-    $(window).on('load', function() {
-        componentResize();
-        draw(clientY); // initialise canvas
-        initNavi();
-        routing();
-    });
+  $(window).on('load', function() {
+    componentResize();
+    drawDesktop(); // initialise canvas
+    initNav();
+    routing();
+  });
 
-    window.addEventListener('mousemove', function(e) {
-        clientY = e.clientY;
-        draw();
+  window.addEventListener('resize', componentResize);
+
+  window.addEventListener('mousemove', function(e) {
+    clientY = e.clientY;
+    drawDesktop();
+  })
+
+  $('#pi-symbol').on('click', function() {
+    location.hash = 'pi';
+    routing();
+  })
+
+  function componentResize(){
+    if(window.innerWidth > 520) {
+        drawDesktop();
+    } else {
+        drawMobile();
+    }
+  }
+
+  function drawMobile() {
+    var mobileHeaderCanvas = document.getElementById('mobile-header-canvas');
+    var mobileHeaderCtx = mobileHeaderCanvas.getContext('2d');
+    var imgBuoyMobile = document.getElementById('img-buoy-mobile');
+
+    mobileHeaderCanvas.width = window.innerWidth;
+    mobileHeaderCanvas.height = 200;
+
+    mobileHeaderCtx.clearRect(0, 0, window.innerWidth, 200);
+    mobileHeaderCtx.drawImage(imgBuoyMobile, (window.innerWidth - window.innerWidth*0.32), 95);
+  }
+
+  function drawDesktop() {
+    var imgBuoy = document.getElementById('img-buoy');
+    var imgAscent = document.getElementById('img-ascent');
+    var imgDescent = document.getElementById('img-descent');
+    var imgBottomPlate = document.getElementById('img-bottom-plate');
+    var desktopNavCanvas = document.getElementById('desktop-nav-canvas');
+    var desktopNavCtx = desktopNavCanvas.getContext('2d');
+
+    desktopNavCanvas.width = 250;
+    desktopNavCanvas.height = window.innerHeight;
+
+    desktopNavCtx.clearRect(0, 0, desktopNavCanvas.width, window.innerHeight);
+
+    // Bottom plate
+    desktopNavCtx.globalAlpha = 0.1;
+    desktopNavCtx.drawImage(imgBottomPlate, 104, window.innerHeight - 98);
+
+    // Lanyard
+    var guideLine = desktopNavCtx.createLinearGradient(118, 129, 122, window.innerHeight - 87);
+    guideLine.addColorStop(0, '#ebebeb');
+    guideLine.addColorStop(1, '#00426e');
+    desktopNavCtx.fillStyle = guideLine;
+    desktopNavCtx.globalAlpha = 0.7;
+    desktopNavCtx.fillRect(123, 129, 2, window.innerHeight - 129 - 87);
+
+    // My mark
+    desktopNavCtx.beginPath();
+    desktopNavCtx.arc(124, 324, 5, 0, 2 * Math.PI, false);
+    desktopNavCtx.fillStyle = '#ff9900';
+    desktopNavCtx.globalAlpha = 1;
+    desktopNavCtx.fill();
+
+    // Buoy
+    desktopNavCtx.drawImage(imgBuoy, 63, 30);
+
+    // Diver
+    desktopNavCtx.globalAlpha = 1 - (clientY / window.innerHeight).toFixed(1);
+    if(clientY > diverY) {
+      if(clientY < window.innerHeight - 220) {
+        desktopNavCtx.drawImage(imgDescent, 30, clientY);
+        diverY = clientY - 1;
+      } else {
+        desktopNavCtx.drawImage(imgDescent, 30, window.innerHeight - 220);
+        diverY = window.innerHeight - 220;
+      }
+    } else {
+      if(clientY > 150) {
+        desktopNavCtx.drawImage(imgAscent, 40, clientY);
+        diverY = clientY;
+      } else {
+        desktopNavCtx.drawImage(imgAscent, 40, 150);
+        diverY = 150;
+      }
+    }
+  }
+
+  function initNav() {
+    $('nav ul li a').on('click', function(e) {
+      $('main article').load('views/' + e.target.hash.replace('#', '') + '.html', function() {
+        $(this).scrollTop(0);
+      });
     })
+  }
 
-    window.addEventListener('resize', componentResize);
-
-    $('#pi-symbol').on('click', function() {
-        location.hash = 'pi';
-        routing();
-    })
-
-    // resize nav canvas to full height
-    function componentResize(){
-        canvasNav.height = window.innerHeight;
-        draw();
-        $('main').width($(window).width() - $('nav').width() - 10);
-        $('main').height($(window).height() - 20);
+  function routing() {
+    if(location.hash) {
+      $('main article').load('views/' + location.hash.replace('#', '') + '.html');
+    } else {
+      $('main article').load('views/moi.html');
     }
+  }
 
-    function draw() {
-        ctxNav.clearRect(0, 0, canvasNav.width, canvasNav.height);
-
-        // Bottom plate
-        ctxNav.globalAlpha = 0.1;
-        ctxNav.drawImage(imgPlate, 104, window.innerHeight - 98);
-
-        // Lanyard
-        var guideLine = ctxNav.createLinearGradient(118, 129, 122, window.innerHeight - 87);
-        guideLine.addColorStop(0, '#ebebeb');
-        guideLine.addColorStop(1, '#00426e');
-        ctxNav.fillStyle = guideLine;
-        ctxNav.globalAlpha = 0.7;
-        ctxNav.fillRect(123, 129, 2, window.innerHeight - 129 - 87);
-
-        // My mark
-        ctxNav.beginPath();
-        ctxNav.arc(124, 324, 5, 0, 2 * Math.PI, false);
-        ctxNav.fillStyle = '#ff9900';
-        ctxNav.globalAlpha = 1;
-        ctxNav.fill();
-
-        // Buoy
-        ctxNav.drawImage(imgBuoy, 63, 30);
-
-        // Diver
-        ctxNav.globalAlpha = 1 - (clientY / window.innerHeight).toFixed(1);
-        if(clientY > diverY) {
-            if(clientY < window.innerHeight - 220) {
-                ctxNav.drawImage(imgDive, 30, clientY);
-                diverY = clientY - 1;
-            } else {
-                ctxNav.drawImage(imgDive, 30, window.innerHeight - 220);
-                diverY = window.innerHeight - 220;
-            }
-        } else {
-            if(clientY > 150) {
-                ctxNav.drawImage(imgFemaleAscent, 40, clientY);
-                diverY = clientY;
-            } else {
-                ctxNav.drawImage(imgFemaleAscent, 40, 150);
-                diverY = 150;
-            }
-        }
-    }
-
-    function initNavi() {
-        $('nav ul li a').on('click', function(e) {
-            $('main').load('views/' + e.target.hash.replace('#', '') + '.html', function() {
-                $(this).scrollTop(0);
-            });
-        })
-    }
-
-    function routing() {
-        if(location.hash) {
-            $('main').load('views/' + location.hash.replace('#', '') + '.html');
-        } else {
-            $('main').load('views/moi.html');
-        }
-    }
-
-    function isMobile() {
-        try {
-            document.createEvent("TouchEvent");
-            return true;
-        } catch(e) {
-            return false;
-        }
-    }
+  // function isMobile() {
+  //   try {
+  //     document.createEvent("TouchEvent");
+  //     return true;
+  //   } catch(e) {
+  //     return false;
+  //   }
+  // }
 });
